573109ed0add02d6f8134ddbced33e9f296e06ed
Pablo Brasero <pablo@thoughtbot.com>
2017-04-04 16:57:03 +0100

    Random changes to list

--- glim-mer/src/ui/components/sample-list/component.ts
+++ glim-mer/src/ui/components/sample-list/component.ts
@@ -10,12 +10,23 @@ export default class SampleList extends Component {
 
   refresh() {
     let newList = []
+
     if (this.itemList.length == 0) {
       let id = 1;
       while (id <= 10000) {
-        newList.push({id, name: "Item #" + 1});
+        newList.push({id, name: "Item #" + id});
         id++;
       }
+    }
+    else {
+      newList = this.itemList.map((item) => {
+        if (Math.random() > 0.9) {
+          return Object.assign({}, item, { name: item.name + "0" });
+        }
+        else {
+          return item;
+        }
+      });
     }
 
     this.itemList = newList;
