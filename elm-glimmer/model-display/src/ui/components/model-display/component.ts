5c0ff364f1a8c4956c09ae689c9bd0df3355a442
Pablo Brasero <pablo@thoughtbot.com>
2017-04-07 15:14:15 +0100

    Basic render and comms

--- elm-glimmer/model-display/src/ui/components/model-display/component.ts
+++ elm-glimmer/model-display/src/ui/components/model-display/component.ts
@@ -1,5 +1,19 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from '@glimmer/component';
 
 export default class ModelDisplay extends Component {
+  @tracked itemList = [];
 
-}
+  @tracked('itemList')
+  get buttonText() {
+    return this.itemList.length == 0 ? "Populate" : "Refresh";
+  }
+
+  refresh() {
+    let msg = {
+      origin: 'view',
+      target: 'core',
+      action: 'refresh',
+    }
+    window.postMessage(msg, '*');
+  }
+};
