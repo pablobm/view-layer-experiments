1bc1e9c6132e09cf0ead0ec9c0675db424c0434c
Pablo Brasero <pablo@thoughtbot.com>
2017-06-02 15:12:17 +0100

    Skeleton of Glimmer component

--- src/tb-blog-post/model-display/src/ui/components/model-display/component.ts
+++ src/tb-blog-post/model-display/src/ui/components/model-display/component.ts
@@ -1,5 +1,13 @@
-import Component from "@glimmer/component";
+import Component, { tracked } from '@glimmer/component';
 
 export default class ModelDisplay extends Component {
+  @tracked model = null;
 
+  increment() {
+    console.log("Increment");
+  }
+
+  decrement() {
+    console.log("Decrement");
+  }
 }
